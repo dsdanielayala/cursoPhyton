849e96f78405a766c881ebfcbdd1e90e0a5b79f7
Daniel Ayala <djdanielayalads99@gmail.com>
2019-04-04 20:28:39 -0500

    proyecto agregado
    se agrega ejemplos de condicionales en python

---
+++ main.py
@@ -0,0 +1,26 @@
+"""x=int(input("Ingrese dato: "))
+y=int(input("Ingrese dato: "))
+print(x+y)
+#comentarios
+
+a=3
+
+if a>3:
+    print("hola")
+    print("mundo")
+elif a<5:
+    print("No es menor")
+else:
+    print("No es mayor")"""
+a=3
+b=4
+""" while a==7:
+    print("Estoy en el while")
+    a=int(input("Ingrese el valor de a: "))
+    if a !=7:
+        break """
+
+if a==3 and b==4:
+    print("es correcto")
+
+
